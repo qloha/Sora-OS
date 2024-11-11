144e242d3f482eb0db2564187f7a44237afee0d4
qloha <172928329+qloha@users.noreply.github.com>
2024-11-11 08:58:01 -0800

    syah

--- backend/userdata.js
+++ backend/userdata.js
@@ -18,5 +18,10 @@ module.exports = [
         username: 'qloha',
         password: 'ITSdefintelyqloha!',
         preferences: { background: '/assets/img/background.png' }
+    },
+    {
+        username: 'Hellsan',
+        password: 'baguettecroissantouioui',
+        preferences: { background: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/472292a2-8cba-4ccf-8403-ded0c3d6d3ad/dgltlib-19260c25-5b62-424e-841b-bd48145b6bb3.jpg/v1/fill/w_894,h_894,q_70,strp/hello_kitty__ai_images___2_by_kittykun123_dgltlib-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcLzQ3MjI5MmEyLThjYmEtNGNjZi04NDAzLWRlZDBjM2Q2ZDNhZFwvZGdsdGxpYi0xOTI2MGMyNS01YjYyLTQyNGUtODQxYi1iZDQ4MTQ1YjZiYjMuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.Sep3f8YfWY808eUbVs8xd91TIvaS5Q8zR1-ztsEETm4' }
     }
 ];
